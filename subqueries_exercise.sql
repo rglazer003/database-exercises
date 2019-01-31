164d0d2e1c68fb78b3a934d08dc0900f65999388
Ryan Glazer <rglazer003@gmail.com>
2019-01-31 16:08:15 -0600

    Finished bonuses

--- subqueries_exercise.sql
+++ subqueries_exercise.sql
@@ -10,3 +10,9 @@ select first_name, last_name
 from employees
 where gender = 'F'
   and emp_no in (select emp_no from dept_manager where to_date > now());
+select dept_name
+from departments
+where dept_no in (select dept_no
+from dept_manager
+where emp_no in (select emp_no from employees where gender = 'F' and to_date > now()));
+select first_name, last_name from employees where emp_no in (select emp_no from salaries where salary in (select max(salary) from salaries));
